bcbfbf1829a6ee33eb0b9d1f1833796ad65ac3bd
forengeit345 <timophey2907@gmail.com>
2025-09-21 12:44:27 +0500

    Task3

---
+++ Tyuiu.BukinTK.Sprint1.Task3.V3.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.BukinTK.Sprint1.Task3.V3.Lib;
+
+namespace Tyuiu.BukinTK.Sprint1.Task3.V3.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValisExpression()
+        {
+            DataService ds = new DataService();
+            double x = 1;
+            double y = 2;
+            double z = 3;
+            double res = ds.ParallelepipedVolume(x, y, z);
+            Assert.AreEqual(6.0, res);
+        }
+    }
+}
---
+++ Tyuiu.BukinTK.Sprint1.Task3.V3/Program.cs
@@ -0,0 +1,43 @@
+﻿using Tyuiu.BukinTK.Sprint1.Task3.V3.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Спринт #1 | Выполнил: Букин Т.К. | ИСПБ-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #1                                                               *");
+        Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+        Console.WriteLine("* Задание #3                                                              *");
+        Console.WriteLine("* Вариант #3                                                              *");
+        Console.WriteLine("* Выполнил: Букин Тимофей Константинович | ИСПБ-25-1                      *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+        Console.WriteLine("* выполняет указанные расчёты и печатает результат на экране.             *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        double x, y, z;
+
+        Console.WriteLine("Введите длинну стороны параллелепипеда:");
+        x = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Введите ширину стороны параллелепипеда:");
+        y = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Введите высоту стороны параллелепипеда:");
+        z = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine(ds.ParallelepipedVolume(x, y, z) + "см.куб.");
+
+        Console.ReadLine();
+    }
+}
