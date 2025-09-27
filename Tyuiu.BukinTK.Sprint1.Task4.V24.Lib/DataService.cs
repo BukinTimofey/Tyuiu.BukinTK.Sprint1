378a06a7533466700e02fd71eb5fb1a4a1178797
forengeit345 <timophey2907@gmail.com>
2025-09-27 14:17:40 +0500

    Task4_1

--- Tyuiu.BukinTK.Sprint1.Task4.V24.Lib/DataService.cs
+++ Tyuiu.BukinTK.Sprint1.Task4.V24.Lib/DataService.cs
@@ -6,7 +6,7 @@ namespace Tyuiu.BukinTK.Sprint1.Task4.V24.Lib
     {
         public double Calculate(double x, double y)
         {
-            double res = Math.Log(x * y) / (x + Math.Sqrt(2 * Math.Pow(y, 2)));
+            double res = Math.Log(x) * y / (x + Math.Sqrt(2 * Math.Pow(y, 2)));
             res = Math.Round(res, 3);
             return res;
         }
