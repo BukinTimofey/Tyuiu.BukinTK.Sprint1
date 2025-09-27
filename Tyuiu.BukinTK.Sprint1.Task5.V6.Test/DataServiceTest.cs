e77d2d3e23f22a163495e45acf7f42a551670060
forengeit345 <timophey2907@gmail.com>
2025-09-27 14:52:48 +0500

    Task5

---
+++ Tyuiu.BukinTK.Sprint1.Task5.V6.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.BukinTK.Sprint1.Task5.V6.Lib;
+
+namespace Tyuiu.BukinTK.Sprint1.Task5.V6.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            int x = 10;
+            double res = ds.Calculate(x);
+            Assert.AreEqual(3, res);
+        }
+    }
+}
---
+++ Tyuiu.BukinTK.Sprint1.Task5.V6/Program.cs
@@ -0,0 +1,40 @@
+﻿using Tyuiu.BukinTK.Sprint1.Task5.V6.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Спринт #1 | Выполнил: Букин Т.К. | ИСПБ-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #1                                                               *");
+        Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+        Console.WriteLine("* Задание #5                                                              *");
+        Console.WriteLine("* Вариант #6                                                              *");
+        Console.WriteLine("* Выполнил: Букин Тимофей Константинович | ИСПБ-25-1                      *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу, которая решает следующую задачу:                    *");
+        Console.WriteLine("* Пусть k – целое от 1 до 365. Присвоить целой переменной n значение      *");
+        Console.WriteLine("* 1,2,...,7 в зависимости от того, на какой день недели (понедельник,     *");
+        Console.WriteLine("* вторник,..., воскресенье) приходится k-й день невисокосного года в      *");
+        Console.WriteLine("* котором 1 января – понедельник.                                         *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        int x;
+
+        Console.WriteLine("Введите день года от 1 до 365:");
+        x = Convert.ToInt16(Console.ReadLine());
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ ДЕНЬ НЕДЕЛИ:                                                  *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine(ds.Calculate(x));
+
+        Console.ReadLine();
+    }
+}
